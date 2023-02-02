076968f24d4fdaf66980973f515445f59d4a5c09
MiguelMendoza22 <jesusmiguelmm@ufps.edu.co>
2023-02-02 10:40:31 -0500

    Comentario inicial.

---
+++ src/main/java/com/co/sofka/appterminaltransporte/AppTerminalTransporteApplication.java
@@ -0,0 +1,13 @@
+package com.co.sofka.appterminaltransporte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppTerminalTransporteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppTerminalTransporteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/co/sofka/appterminaltransporte/AppTerminalTransporteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.co.sofka.appterminaltransporte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTerminalTransporteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
